e2850d6c31d9b3647c326dabb8fdfd65c4dcf88b
aldijoko <joko@volantis.io>
2022-05-30 21:37:27 +0700

    update for header UI

---
+++ src/lib/components/home/index.svelte.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<div class="pt-10">
+tess
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://github.com/aldijoko">Aldi Joko</a></p>
+</footer>
+
+<style>
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
